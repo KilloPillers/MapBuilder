03fa15c6562d9243806dfe110d1d62b3cbdbfb13
jalvarez957 <87673004+KilloPillers@users.noreply.github.com>
2023-12-17 23:26:10 -0800

    Converted Map.py to have a class structure

--- Map.py
+++ Map.py
@@ -1,123 +1,150 @@
 import pygame
 import sys
+import tkinter as tk
 from ButtonClass import ButtonClass
-
-Grid_Width = 20
-Grid_Height = 20
-ButtonGrid = [0 for _ in range(Grid_Width*Grid_Height)]
-
-pygame.init()
 
 BACKGROUND_COLOR = (49, 51, 55)
 DEFAULT_BUTTON_COLOR = (30, 31, 34)
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
 
-for x in range(Grid_Width):
-    for y in range(Grid_Height):
-        ButtonGrid[(y*Grid_Height)+x] = ButtonClass(75*x, 75*y, 75, 75, DEFAULT_BUTTON_COLOR, "5")
+class PygameInterface():
+    def __init__(self, width, height, grid_width, grid_height):
+        pygame.init()
+        self.grid_width = grid_width
+        self.grid_height = grid_height
+        
+        self.ButtonGrid = [0 for _ in range(self.grid_width*self.grid_height)]
+        for x in range(self.grid_width):
+            for y in range(self.grid_height):
+                self.ButtonGrid[(y*self.grid_height)+x] = ButtonClass(75*x, 75*y, 75, 75, DEFAULT_BUTTON_COLOR, "5")
+        
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        
+        self.dragging = False
+        self.selecting = False
+        
+        self.start_pos = None
+        self.end_pos = None
+        
+        self.camera_offset = [0,0]
+        self.prev_mouse_pos = None
+        self.dragging_camera = False
+        self.zoom_factor = 1.0
+        self.selection_surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
 
-dragging = False
-selecting = False
-start_pos = None
-end_pos = None
-camera_offset = [0,0]
-dragging_camera = False
-zoom_factor = 1.0
-selection_surface = pygame.Surface((800, 600), pygame.SRCALPHA)
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:  # 1 = left mouse button, 2 = middle, 3 = right
-                dragging = True
-                selecting = True
-                start_pos = event.pos
-                for button in ButtonGrid:
-                    if button.zoom_rect.collidepoint(event.pos):
-                        button.color = (0, 255, 0)
-            if event.button == 3:
-                dragging = True
-                selecting = False
-                start_pos = event.pos
-                for button in ButtonGrid:
-                    if button.zoom_rect.collidepoint(event.pos):
-                        button.color = DEFAULT_BUTTON_COLOR
-            if event.button == 2: # moving camera_pos
-                dragging_camera = True
-                prev_mouse_pos = pygame.mouse.get_pos()
-            # Zooming
-            if event.button == 4:  # Scroll up
-                zoom_factor *= 1.01  # Increase zoom
-                for x in range(Grid_Width):
-                    for y in range(Grid_Height):
-                        ButtonGrid[(y*Grid_Height)+x].zoom(zoom_factor, pygame.mouse.get_pos())
-            elif event.button == 5:  # Scroll down
-                zoom_factor /= 1.01  # Decrease zoom
-                for x in range(Grid_Width):
-                    for y in range(Grid_Height):
-                        ButtonGrid[(y*Grid_Height)+x].zoom(zoom_factor, pygame.mouse.get_pos())
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # 1 = left mouse button, 2 = middle, 3 = right
+                    self.dragging = True
+                    self.selecting = True
+                    self.start_pos = event.pos
+                    for button in self.ButtonGrid:
+                        if button.zoom_rect.collidepoint(event.pos):
+                            button.color = (0, 255, 0)
                 
-        if event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                dragging = False
-                start_pos = None
-                end_pos = None
-            if event.button == 2:
-                dragging_camera = False
-            if event.button == 3:
-                dragging = False
-                start_pos = None
-                end_pos = None
+                if event.button == 3:
+                    self.dragging = True
+                    self.selecting = False
+                    self.start_pos = event.pos
+                    for button in self.ButtonGrid:
+                        if button.zoom_rect.collidepoint(event.pos):
+                            button.color = DEFAULT_BUTTON_COLOR
+                
+                if event.button == 2: # moving camera_pos
+                    self.dragging_camera = True
+                    self.prev_mouse_pos = pygame.mouse.get_pos()
+                
+                # Zooming
+                if event.button == 4:  # Scroll up
+                    self.zoom_factor *= 1.01  # Increase zoom
+                    for x in range(self.grid_width):
+                        for y in range(self.grid_height):
+                            self.ButtonGrid[(y*self.grid_height)+x].zoom(self.zoom_factor, pygame.mouse.get_pos())
+                elif event.button == 5:  # Scroll down
+                    self.zoom_factor /= 1.01  # Decrease zoom
+                    for x in range(self.grid_width):
+                        for y in range(self.grid_height):
+                            self.ButtonGrid[(y*self.grid_height)+x].zoom(self.zoom_factor, pygame.mouse.get_pos())
+                    
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    self.dragging = False
+                    self.start_pos = None
+                    self.end_pos = None
+                if event.button == 2:
+                    self.dragging_camera = False
+                if event.button == 3:
+                    self.dragging = False
+                    self.start_pos = None
+                    self.end_pos = None
 
-        if event.type == pygame.MOUSEMOTION:
-            # Code for highlighting buttons on hover
-            #for button in ButtonGrid:
-            #    if button.isOver(event.pos):
-            #        button.color = (0, 0, 255)
-            #    else:
-            #        button.color = (255, 0, 0)
-            if dragging:
-               end_pos = event.pos
-            if dragging_camera:
-                current_mouse_pos = pygame.mouse.get_pos()
-                dx = current_mouse_pos[0] - prev_mouse_pos[0]
-                dy = current_mouse_pos[1] - prev_mouse_pos[1]
-                camera_offset[0] += dx
-                camera_offset[1] += dy
-                prev_mouse_pos = current_mouse_pos
-                for x in range(Grid_Width):
-                    for y in range(Grid_Height):
-                        #adjut position of button based on camera_pos
-                        ButtonGrid[(y*Grid_Height)+x].zoom_rect.move_ip(dx, dy)
-                        ButtonGrid[(y*Grid_Height)+x].rect.move_ip(dx, dy)
-                # Update button position
-    
-    screen.fill(BACKGROUND_COLOR)  # Fill the screen with white
-    for x in range(Grid_Width):
-        for y in range(Grid_Height):
-            ButtonGrid[(y*Grid_Height)+x].draw(screen)
+            if event.type == pygame.MOUSEMOTION:
+                # Code for highlighting buttons on hover
+                #for button in ButtonGrid:
+                #    if button.isOver(event.pos):
+                #        button.color = (0, 0, 255)
+                #    else:
+                #        button.color = (255, 0, 0)
+                if self.dragging:
+                   self.end_pos = event.pos
+                if self.dragging_camera:
+                    current_mouse_pos = pygame.mouse.get_pos()
+                    dx = current_mouse_pos[0] - self.prev_mouse_pos[0]
+                    dy = current_mouse_pos[1] - self.prev_mouse_pos[1]
+                    self.camera_offset[0] += dx
+                    self.camera_offset[1] += dy
+                    self.prev_mouse_pos = current_mouse_pos
+                    for x in range(self.grid_width):
+                        for y in range(self.grid_height):
+                            #adjut position of button based on camera_pos
+                            self.ButtonGrid[(y*self.grid_width)+x].zoom_rect.move_ip(dx, dy)
+                            self.ButtonGrid[(y*self.grid_height)+x].rect.move_ip(dx, dy) 
 
-    if dragging and start_pos and end_pos:
-        selection_surface.fill((0, 0, 0, 0)) # Clear previous rectangle
+    def update_screen(self):
+        self.screen.fill(BACKGROUND_COLOR)  # Fill the screen with white
+        for x in range(self.grid_width):
+            for y in range(self.grid_height):
+                self.ButtonGrid[(y*self.grid_height)+x].draw(self.screen)
+        if self.dragging and self.start_pos and self.end_pos:
+            self.selection_surface.fill((0, 0, 0, 0)) # Clear previous rectangle
         
-        top_left_x = min(start_pos[0], end_pos[0])
-        top_left_y = min(start_pos[1], end_pos[1])
-        width = abs(end_pos[0] - start_pos[0])
-        height = abs(end_pos[1] - start_pos[1])
-        
-        selection_rect = pygame.Rect(top_left_x, top_left_y, width, height) 
-        pygame.draw.rect(selection_surface, (0, 128, 255, 100), selection_rect)  # RGBA, 100 alpha for transparency
-        screen.blit(selection_surface, (0, 0))
-        
-        for button in ButtonGrid:
-            if selection_rect.colliderect(button.zoom_rect):
-                #change color of button
-                button.color = (0, 255, 0) if selecting else DEFAULT_BUTTON_COLOR
+            top_left_x = min(self.start_pos[0], self.end_pos[0])
+            top_left_y = min(self.start_pos[1], self.end_pos[1])
+            width = abs(self.end_pos[0] - self.start_pos[0])
+            height = abs(self.end_pos[1] - self.start_pos[1])
+            
+            selection_rect = pygame.Rect(top_left_x, top_left_y, width, height) 
+            pygame.draw.rect(self.selection_surface, (0, 128, 255, 100), selection_rect)  # RGBA, 100 alpha for transparency
+            self.screen.blit(self.selection_surface, (0, 0))
+            
+            for button in self.ButtonGrid:
+                if selection_rect.colliderect(button.zoom_rect):
+                    #change color of button
+                    button.color = (0, 255, 0) if self.selecting else DEFAULT_BUTTON_COLOR
+        pygame.display.flip()
 
-    pygame.display.flip()
+
+def tkinter_update():
+    pygame_interface.handle_events()
+    pygame_interface.update_screen()
+    pygame.display.update()
+    root.after(20, tkinter_update)
+
+
+pygame_interface = PygameInterface(800, 600, 10, 10)
+while True:
+    pygame_interface.handle_events()
+    pygame_interface.update_screen()
+    pygame.display.update()
+
+#root = tk.Tk()
+#root.title("Map Editor")
+#root.after(20, tkinter_update)
+#root.mainloop()
+
